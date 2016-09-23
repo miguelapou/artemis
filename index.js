2d3ab047f1dfb765660369632a37b70d6fb1f3a3
shway250 <jackson_duhon@yahoo.com>
2016-09-23 10:00:12 -0700

    made changes so we can use mongo on heroku

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var app = express();
 // mongoose models and connection
 var mongoose = require('mongoose');
 var User = require('./models/user');
-mongoose.connect('mongodb://localhost/jobs');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mydbname');
 
 // decode POST data in JSON and URL encoded formats
 app.use(bodyParser.json());
